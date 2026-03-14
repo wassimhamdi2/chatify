f9f8f7524c367a9684d8bb70139cbec4f71427ad
wassimhamdi2 <whamdi617@gmail.com>
2026-03-14 15:22:04 +0100

    fix problem of path  for deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
 import authRouters from "./routers/auth.router.js";
 import messageRouters from "./routers/message.router.js";
-import path from "path";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
-const __dirname = path.resolve();
+const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-
+app.use(express.json());
 app.use("/api/auth", authRouters);
 app.use("/api/messages", messageRouters);
 
-//make ready for deployment
 if (process.env.NODE_ENV === "production") {
     const distPath = path.join(__dirname, "../../frontend/dist");
     console.log("Serving static from:", distPath);
-    
+
     app.use(express.static(distPath));
 
     app.get("*", (_, res) => {
         res.sendFile(path.join(distPath, "index.html"));
     });
-
 }
-app.listen(PORT, () => console.log("server is running on port " + PORT));
 
+app.listen(PORT, () => console.log("server is running on port " + PORT));
