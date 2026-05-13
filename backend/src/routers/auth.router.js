46a9f5ef82ee55eb60b224143b932ae4f6c8ee3d
wassimhamdi2 <whamdi617@gmail.com>
2026-05-13 10:03:55 +0100

    add arcjet for protection counter over requests and bots for all routes !!

--- backend/src/routers/auth.router.js
+++ backend/src/routers/auth.router.js
@@ -6,11 +6,11 @@ import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-//router.use(arcjetProtection);
+router.use(arcjetProtection);
 
-router.get("/test", arcjetProtection, (req, res) => {
-  res.status(200).json({ message: "Test route" });
-});
+// router.get("/test", arcjetProtection, (req, res) => {
+//   res.status(200).json({ message: "Test route" });
+// });
 router.post("/signup", signup);
 router.post("/login", login );
 router.post("/logout", logout);
