2767e815932ecaab2618d338eb2209b3c0062e8e
wassimhamdi2 <whamdi617@gmail.com>
2026-05-13 09:54:00 +0100

    add arcjet for protection counter over requests and bots

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -18,5 +18,8 @@ export const ENV = {
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_KEY,
 
+  ARCJET_KEY: process.env.ARCJET_KEY,
+  ARCJET_ENV: process.env.ARCJET_ENV,
+
 };
 
--- backend/src/routers/auth.router.js
+++ backend/src/routers/auth.router.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+
+
 const router = express.Router();
 
+//router.use(arcjetProtection);
 
-
-
+router.get("/test", arcjetProtection, (req, res) => {
+  res.status(200).json({ message: "Test route" });
+});
 router.post("/signup", signup);
 router.post("/login", login );
 router.post("/logout", logout);
